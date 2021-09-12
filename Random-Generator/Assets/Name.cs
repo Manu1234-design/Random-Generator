db6660d5f1ea1ae517fbfde4765c23ede5b58338
DESKTOP-PL3AINM\GOVT ITI WOMENS <peringhatmanu@gmail.com>
2021-09-12 18:01:40 +0530

    Random Generator First Editon

---
+++ Random-Generator/Assets/Name.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Name : MonoBehaviour
+{
+    
+    public Text nameText;
+
+    public void PickRandomFromList(){
+        string[] students = new string[] { "Manu", "Rajeev", "Adithyan"};
+        string RandomName = students[Random.Range(0, students.Length)];
+        nameText.text = RandomName;
+    }
+}
---
+++ Random-Generator/Assets/Number.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Number : MonoBehaviour
+{
+    public Text numberText;
+    
+
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    public void Ok(){
+        PickRandom(41);
+    }
+
+    private void PickRandom (int maxInt){
+        int randomNum = Random.Range(1, maxInt+1);
+        numberText.text = randomNum.ToString();
+
+    }
+
+    
+}
